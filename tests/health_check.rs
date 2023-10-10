c01dffb0318e567315ce69c2ec1253058374ea4f
Alexandre RAY-BERNAT <alexandre.ray-bernat@outscale.com>
2023-10-10 10:01:40 +0200

    Integration etsts - First implemaentation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -13,7 +13,49 @@ async fn health_check_works() {
         .await
         .expect("Failed to execute request.");
     assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length()); }
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=aa%20%%bb&email=jozédubois%40@gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address)) .header("Content-Type", "application/x-www-form-urlencoded") .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=aa%20bb", "Email is missing"),
+        ("email=jozédubois%40gmail.com", "Name is missing"),
+        ("", "Email and Name are missing")
+    ];
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address)) .header("Content-Type", "application/x-www-form-urlencoded") .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        assert_eq!( 400,
+                    response.status().as_u16(),
+                    // Additional customised error message on test failure
+                    "The API did not fail with 400 Bad Request when the payload was {}.",
+                    error_message
+        ); }
+}
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
