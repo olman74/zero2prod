b2771532073a93ca640dbc43c36acccbf1e2eaba
Alexandre RAY-BERNAT <alexandre.ray-bernat@outscale.com>
2023-10-25 10:00:53 +0200

    Added first migration

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,20 @@ use std::net::TcpListener;
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| { App::new()
         .route("/health_check", web::get().to(health_check) )
+        .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
