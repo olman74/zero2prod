994bddf47a17a8669f83d587824a79fb0bd1ecc7
Alexandre RAY-BERNAT <alexandre.ray-bernat@outscale.com>
2023-11-03 09:35:45 +0100

    Starting tracing implementation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,14 +12,16 @@ pub struct FormData {
 
 pub async fn subscribe(_form: web::Form<FormData>, _connection: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "Request id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        _form.email,
-        _form.name
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email: %_form.email,
+        subscriber_name: %_form.name
     );
 
-    log::info!("Request id {} - Saving new subscriber details in the database", request_id);
+    let _request_span_guard = request_span.enter();
+
+    tracing::info!("Request id {} - Saving new subscriber details in the database", request_id);
 
     match sqlx::query!(
         r#"
@@ -34,11 +36,11 @@ pub async fn subscribe(_form: web::Form<FormData>, _connection: web::Data<PgPool
         .execute(_connection.get_ref())
         .await {
         Ok(_) => {
-            log::info!("Request id {} - New subscriber details have been saved", request_id);
+            tracing::info!("Request id {} - New subscriber details have been saved", request_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("Request id {} - Failed to execute query: {:?}", request_id, e);
+            tracing::error!("Request id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
