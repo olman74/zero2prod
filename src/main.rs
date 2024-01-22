671ad35b8f04b909ad386ed4d3db5abf3ee3c845
Alexandre RAY-BERNAT <alexandre.ray-bernat@outscale.com>
2024-01-22 10:55:10 +0100

    Last modifications

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 use zero2prod::startup::run;
 use std::net::TcpListener;
-use sqlx::{PgPool};
 use zero2prod::configuration::get_configuration;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-
     let subscriber = get_subscriber("zero2Prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration");
-    let address = format!("{}:{}",configuration.application.host,  configuration.application.port);
-    let connection_poool = PgPool::connect_lazy(&configuration.database.connection_string()
-        .expose_secret())
-        .expect("Failed to connect to Postgres");
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
+    let connection_pool = PgPoolOptions::new().connect_timeout(std::time::Duration::from_secs(2)
+    ).connect_lazy_with(configuration.database.with_db());
+
+
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_poool)?.await;
+    run(listener, connection_pool)?.await;
     Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -76,6 +76,27 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         ); }
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new(); let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address)) .header("Content-Type", "application/x-www-form-urlencoded") .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        assert_eq!( 200,
+                    response.status().as_u16(),
+                    "The API did not return a 200 OK when the payload was {}.",
+                    description
+        ); }
+}
+
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
     let subscriber_name = "test".to_string();
@@ -118,20 +139,19 @@ async fn spawn_app() -> TestApp {
 
 }
 
-pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(&config.connection_string_without_db()
-        .expose_secret())
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool { // Create database
+    let mut connection = PgConnection::connect_with(&config.without_db())
         .await
         .expect("Failed to connect to Postgres");
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str()) .await
         .expect("Failed to create database.");
 
-    let connection_pool = PgPool::connect(&config.connection_string().expose_secret()) .await
+// Migrate database
+    let connection_pool = PgPool::connect_with(config.with_db()) .await
         .expect("Failed to connect to Postgres."); sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
         .expect("Failed to migrate the database");
     connection_pool
-
 }
